5a179da031f38f4144c4c618de6b58cc10e0a1d3
CoinerLo <coiner4677@gmail.com>
2022-05-25 15:53:37 +0300

    fix console

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -4,7 +4,7 @@ import AuthContext from '../context/AuthContext';
 
 const ProfilePage = () => {
     const { user } = useContext(AuthContext);
-    console.log(user)
+
     return (
         <main className="d-flex flex-column align-content-center text-center">
             <h1 className="h1 mt-5">Профиль пользователя</h1>
